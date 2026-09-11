6f036cc708166e104b8f539b32fec2cfbe6c54d9
scopy132 <opeyemiajetunmobi9@gmail.com>
2026-09-12 00:12:53 +0100

    Delete mrp_multi_product_pro directory

--- mrp_multi_product_pro/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import stock_move
-from . import mrp_production_product_line
-from . import mrp_production
-from . import sale_order
--- mrp_multi_product_pro/wizard/mrp_production_merge.py
+++
@@ -1,4 +0,0 @@
-# The multi-product merge logic overrides mrp.production.action_merge()
-# directly (see models/mrp_production.py) so it plugs into Odoo's native
-# "Merge" action already available from the Manufacturing Orders list view.
-# No separate transient wizard model is needed.
